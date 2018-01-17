f97984b2d3d4de9b36d77bf5091c1fc5bb30fee2
xiaoluoboding <xiaoluoboding@gmail.com>
2018-01-17 16:47:20 +0800

    更新 - 图表随机配色

---
+++ demos/data/color.js
@@ -0,0 +1,65 @@
+const defaultColor = [
+  '#5282e4',
+  '#40b17e',
+  '#ffd600',
+  '#f88d48',
+  '#2ebce2',
+  '#f35352',
+  '#ce62d6',
+  '#8954d4',
+  '#5257b8',
+  '#d42d6b'
+]
+
+const coldStyle = [
+  '#4c67ca',
+  '#67b4d1',
+  '#888fcc',
+  '#afdce3',
+  '#74638f',
+  '#575a75'
+]
+
+const retroStyle = [
+  '#ba4a4b',
+  '#297389',
+  '#c99129',
+  '#7b4f71',
+  '#333d80',
+  '#c3684a',
+  '#88c4dd',
+  '#d6aa85',
+  '#2a3537',
+  '#79641a'
+]
+
+const summerStyle = [
+  '#207722',
+  '#68b133',
+  '#a9de83',
+  '#f63629',
+  '#f66258',
+  '#ffc0af'
+]
+
+const fallStyle = [
+  '#0da6b7',
+  '#6ed3de',
+  '#afdce3',
+  '#ea6f2b',
+  '#ffc151',
+  '#ebe287'
+]
+
+const purpleStyle = [
+  '#81298e',
+  '#589878',
+  '#5a3272',
+  '#344a43',
+  '#9b5ab8',
+  '#a9bab2'
+]
+
+const colors = [defaultColor, coldStyle, retroStyle, summerStyle, fallStyle, purpleStyle]
+
+export default colors[Math.floor(Math.random() * colors.length)]
