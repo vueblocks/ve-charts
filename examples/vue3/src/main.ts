9cbb2d9e5e5322e387f85361b14ce567ff7956f8
xiaoluoboding <xiaoluoboding@gmail.com>
2021-03-31 18:57:27 +0800

    docs: update examples

--- examples/vue3/src/main.ts
+++ examples/vue3/src/main.ts
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { VeBarChart, VeLineChart } from 've-charts'
+import VeCharts from 've-charts'
 
 const app = createApp(App)
 
-app.component(VeBarChart.name, VeBarChart)
-app.component(VeLineChart.name, VeLineChart)
+app.use(VeCharts)
 
 app.mount('#app')
